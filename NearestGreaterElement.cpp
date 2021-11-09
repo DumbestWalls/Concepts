76ea921aa5e51d5e4d727e1d7278d46013a67ade
Dumbest <88792486+DumbestWalls@users.noreply.github.com>
2021-11-09 08:13:12 +0530

    Nearest Greater Element (STACK PROBLEM)

---
+++ NearestGreaterElement.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<array>
+
+using namespace std;
+
+int main()
+{
+    array< int,4> arr{1,3,2,4};
+    vector<int> v;
+    stack<int> s;
+    
+    for(int i=arr.size()-1;i>=0;i--)
+    {
+        if(s.size()==0)
+        {
+            v.push_back(-1);
+        }
+        else if(s.size()>0 && s.top()>arr[i])
+        {
+            v.push_back(s.top());
+        }
+        else{
+            while(s.size()>0 && s.top()<=arr[i])
+            {
+                s.pop();
+            }
+            if(s.size()==0) v.push_back(-1);
+            else v.push_back(s.top());
+        }
+        
+        s.push(arr[i]);
+    }
+    
+    for(int i=v.size()-1;i>=0;i--)
+    {
+        cout << v.at(i) << ' ';
+    }
+}
